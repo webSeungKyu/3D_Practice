fb0932c29b599e5e904e750cd78eef39daf2b3b3
webSeungKyu <abcsksk1@gmail.com>
2024-03-13 17:14:25 +0900

    UI, 조이스틱, 애니메이션 연습

--- ActionGame/Assets/Scripts/Controller/TouchPad.cs
+++ ActionGame/Assets/Scripts/Controller/TouchPad.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class TouchPad : MonoBehaviour
 {
@@ -19,5 +20,130 @@ public class TouchPad : MonoBehaviour
 
     //버튼 눌렸는지를 체크하는 변수
     private bool _buttonPressed = false;
+
+
+    private void Start()
+    {
+        _touchPad = GetComponent<RectTransform>();
+
+        _startPos = _touchPad.position;
+    }
+
+    public void ButtonUp()
+    {
+        Debug.Log("ButtonUp");
+        _buttonPressed = true;
+        HandleInput(_startPos);
+    }
+
+    public void ButtonDown()
+    {
+        Debug.Log("ButtonDown");
+        _buttonPressed = true;
+    }
+
+
+    private void FixedUpdate()
+    {
+        //일반적인 경우 터치패드로 작업합니다. (모바일)
+        HandleTouchInput();
+
+        //#if는 조건부 컴파일을 구현하기 위한 전처리기
+        //유니티 에디터 / 웹 / 인게임에서 마우스 클릭으로 작업합니다.
+#if UNITY_EDITOR || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_WEBGL
+        HandleInput(Input.mousePosition);
+#endif
+    }
+
+
+    void HandleTouchInput()
+    {
+        int i = 0; //터치 아이디를 매기기 위한 변수
+
+        //터치가 한 번이라도 들어오면 실행하도록
+        if(Input.touchCount > 0)
+        {
+            foreach(Touch touch in Input.touches)
+            {
+                i++; // 터치 번호 증가
+
+                //입력한 터치 값으로 좌표를 계산합니다
+                Vector3 touchPos = new Vector3(touch.position.x, touch.position.y);
+
+                //그 터치가 현재의 방향키 범위 내에 존재하는 경우
+                if(touch.phase == TouchPhase.Began )
+                {
+                    if(touch.position.x <=  _startPos.x + _dragRadius)
+                    {
+                        //이 터치 값을 기준으로 컨트롤러를 조작합니다
+                        _touchId = i;
+                    }
+                }
+
+                //터치 입력이 움직였거나, 가만히 있는 상황일 경우
+                if(touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+                {
+                    //터치 아이디로 지정이 된 상태일 때
+                    if(_touchId == i)
+                    {
+                        HandleInput(touchPos);
+                    }
+                }
+
+                //터치 입력이 끝난 경우
+                if(touch.phase == TouchPhase.Ended)
+                {
+                    if(_touchId == i)
+                    {
+                        _touchId = -1;
+                    }
+                }
+            }
+        }
+    }
+
+    void HandleInput(Vector3 input)
+    {
+        //버튼이 눌려져있는 상황일 경우
+        if(_buttonPressed)
+        {
+            //방향 컨트롤러의 기준 좌표부터 입력박은 좌표가 얼마나 떨어져있는지를 구합니다
+            Vector3 diffVector = (input -  _startPos);
+
+            //sqrMagnitude는 두 점 간의 거리의 제곱에 루트를 한 값
+            //비슷한 개념 Vector3.Distance (연산속도가 느린 편), 대신 건축물처럼 정교한 값을 구할 때 사용
+            // sqrMagnitude는 단순하게 두점 사이의 거리를 구할 때 사용
+            //정확한 거리를 체크하는 게 아닌 값이 크고 작은지만 판단하는 식
+            if (diffVector.sqrMagnitude > _dragRadius * _dragRadius)
+            {
+                diffVector.Normalize();//방향 백터의 거리를 1로 설정합니다. (정규화)
+                //각 위치별 거리를 1로 고정
+
+                //방향 컨트롤러는 최대치만큼 이동합니다
+                _touchPad.position = _startPos + diffVector * _dragRadius;
+            }
+            else
+            {
+                //입력 지점과 기준 좌표가 최대치보다 크지 않을 경우 현재 입력 위치로 방향키를 옮겨줍니다.
+                _touchPad.position = input;
+            }
+        }
+        else
+        {
+            //버튼에서 손이 떼어질 경우, 방향키를 원래 위치로 변경합니다
+            _touchPad.position = _startPos;
+        }
+        //방향키와 기준점의 차이를 계산합니다
+        Vector3 diff = _touchPad.position - _startPos;
+
+        //거리만 나누어 방향을 계산합니다
+        Vector2 normDiff = new Vector3(diff.x / _dragRadius, diff.y / _dragRadius);
+
+        if(_player != null)
+        {
+            _player.OnStickChanged(normDiff);
+        }
+    }
+
 }
 
--- ActionGame/Assets/Scripts/Player/PlayerMovement.cs
+++ ActionGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -43,6 +43,11 @@ public class PlayerMovement : MonoBehaviour
     {
         if (avatar)
         {
+            float back = 1.0f;
+            if(v < 0.0f)
+            {
+                back = -1.0f;
+            }
             avatar.SetFloat("Speed", (h * h) + (v * v));
             Rigidbody rigidbody = GetComponent<Rigidbody>();
 
@@ -56,4 +61,79 @@ public class PlayerMovement : MonoBehaviour
             }
         }
     }
+
+    #region EventTrigger
+    public void OnAttackDown()
+    {
+        attacking = true;
+        avatar.SetBool("Combo", true);
+        StartCoroutine("StartAttack"); // 코루틴을 작동시키는 코드
+        // StartCoroutine(StartAttack()); 이렇게도 사용 가능
+        //코루틴은 Update가 아닌 영역에서 반복적으로 코드가 실행되어야 할 때 사용하면 매우 효과적
+        //Update에서 무분별하게 돌리는 코드를 코루틴으로 전환하면, 자원 관리에 효과적
+        //코루틴은 일정 시간 멈추고 뒤에 움직이는 작업, 특정 조건을 부여해 코드를 실행하는 작업에 용이합니다.
+        //코루틴은 IEnumrator 형태의 함수를 시작합니다
+        //해당 함수 내부에는 반드시 yield return문으 존재해야 합니다.
+
+        //지연 함수로는 Invoke도 있지만, 이건 말그대로 그 시간만큼 지연 후 함수 실행이라 코루틴과는 약간 다름
+        //코루틴은 반복 루틴에서 탈출하고 다시 그 시점으로 돌아오는 것이 가능합니다.
+    }
+
+
+
+    public void OnAttackUp()
+    {
+        avatar.SetBool("Combo", false);
+        attacking = false;
+    }
+
+    //yield문은 다음 요소를 제공하는 키워드입니다
+    IEnumerator StartAttack()
+    {
+        if(Time.time - lastAttackTime > 1.0f)
+        {
+            lastAttackTime = Time.time;
+            while (attacking)
+            {
+                avatar.SetTrigger("AttackStart");
+                yield return new WaitForSeconds(1.0f);
+            }
+        }
+ 
+    }
+
+    public void OnSkillUp()
+    {
+        avatar.SetBool("Skill", false);
+    }
+
+    public void OnDashUp()
+    {
+        dashing = false;
+    }
+
+    /// <summary>
+    /// 버튼 두 번 누르면 나갈 스킬
+    /// </summary>
+    public void OnSkillDown()
+    {
+        if(Time.time - lastSkillTime > 1.0f)
+        {
+            avatar.SetBool("Skill", false);
+        }
+    }
+
+    /// <summary>
+    /// 버튼 한 번 누르면 나갈 스키
+    /// </summary>
+    public void onDashDown()
+    {
+        if (Time.time - lastDashTime > 1.0f)
+        {
+            lastDashTime = Time.time;
+            dashing = true;
+            avatar.SetTrigger("Dash");
+        }
+    }
+    #endregion
 }
