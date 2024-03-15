09da810152945b74ea04ff9a88d32bc1ee065e73
webSeungKyu <abcsksk1@gmail.com>
2024-03-15 09:09:32 +0900

    GetComponent 연습

--- ActionGame/Assets/Scenes/GetComponentAndEnabledExample.cs
+++ ActionGame/Assets/Scenes/GetComponentAndEnabledExample.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class GetComponentAndEnabledExample : MonoBehaviour
 {
+    private Rigidbody rbody;
+    public GameObject box;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>();
+        /*rbody = GetComponent<Rigidbody>();
+        rbody.useGravity = true;*/
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+/*        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("마우스 왼쪽 클릭");
+            box.enabled = false;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("마우스 오른쪽 클릭");
+            box.enabled = true;
+        }*/
     }
 }
