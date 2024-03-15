81a1b92b9fcd03dda43cd9d8802ae8db74819dff
webSeungKyu <abcsksk1@gmail.com>
2024-03-15 11:23:16 +0900

    딕셔너리 연습
             딕셔너리의 핵심
            1. 키를 이용한 값에 대한 접근
            2. 해당 키가 존재하는가에 대한 여부
            3. 키, 값을 각각 분할해 보관할 수 있는가?(리스트 변환)
            4. 딕셔너리는 키의 경우에는 중복을 허용하지 않고, 값은 중복을 허용합니다
            > 따라서 Add 진행할 때, 기존에 있는 키를 다시 Add하는 경우 그 키가 가진 값만 변경

--- ActionGame/Assets/Scenes/DSController.cs
+++ ActionGame/Assets/Scenes/DSController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -36,7 +37,7 @@ public class DSController : MonoBehaviour
         }
 
         //exp.RemoveAll(x => (x % 4) == 0); 
-        exp.Sort( (a,b) => b.CompareTo(a)  );
+        exp.Sort((a, b) => b.CompareTo(a));
 
         for (int i = 0; i < exp.Count; i++)
         {
@@ -58,6 +59,58 @@ public class DSController : MonoBehaviour
          */
 
 
+    }
+
+    public void DSDictionary()
+    {
+        //생성 Dictionary<K, V> 딕셔너리명 = Dictionary<K, V>();
+
+        //생성 및 초기화
+        Dictionary<string, int> items = new Dictionary<string, int>
+        {
+            
+            {"red apple", 10 },
+            {"meat", 100 }
+
+        };
+
+        
+        //기능 추가
+        items.Add("cake", 50);
+
+        if (items.ContainsKey("cake"))
+        {
+            items.Remove("cake");
+        }
+
+        if (items.ContainsValue(100))
+        {
+            Debug.Log("해당 값은 존재합니다.");
+        }
+        /*
+         딕셔너리의 핵심
+        1. 키를 이용한 값에 대한 접근
+        2. 해당 키가 존재하는가에 대한 여부
+        3. 키, 값을 각각 분할해 보관할 수 있는가?(리스트 변환)
+        4. 딕셔너리는 키의 경우에는 중복을 허용하지 않고, 값은 중복을 허용합니다
+        > 따라서 Add 진행할 때, 기존에 있는 키를 다시 Add하는 경우 그 키가 가진 값만 변경
+         */
+
+
+        //딕셔너리의 키 -> 리스트로 바꾸는 기능
+        var KList = new List<string>(items.Keys);
+
+        //딕셔너리의 값 -> 리스트로 바꾸는 기능
+        var KList2 = new List<int>(items.Values);
+
+        //리스트 - > 딕셔너리로 바꾸는법
+        List<int> kToD = new List<int>() { 1, 2, 3, };
+        List<string> vTod = new List<string>() { "a", "b" }; 
+
+        //딕셔너리를 생성하고 Zip 함수를 통해 작업을 진행합니다
+        //키.Zip(값, (k,v) => new {k, v}) 키와 값 하나하나가 {키, 값}의 형태로 묶이게 됩니다.
+        //ToDictionary에 의해 키와 값을 설정합니다. 그리고 딕셔너리의 형태로 반환합니다
+        Dictionary<int, string> newD = kToD.Zip(vTod, (k,v) => new {k, v}).ToDictionary(a => a.k, a=> a.v);
     }
 
     //해당 함수 호출 시 화면에 나와있는 텍스트를 비우는 기능
