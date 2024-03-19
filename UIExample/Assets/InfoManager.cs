87a163907ef104462b6d1ed4940d7140e90a30ac
webSeungKyu <abcsksk1@gmail.com>
2024-03-19 12:17:50 +0900

    UI 연습
    유니티의 특수 폴더
    일반적으로 유니티에서 폴더를 생성할 때 이름은 마음대로 짓는 것이 가능합니다
    
    단 특정 작업에 사용되는 폴더개 몇 개 존재합니다
    
    1. Assets
    유니티 프로젝트에서 사용하는 에셋이 포함되는 폴더입니다.
    대부분의 API 함수들이 이 에셋 폴더를 지정하고 있습니다.
    
    2. Editor
    이 폴더에서는 런타임 스크립트가 아닌 에디터 스크립트를 작업할 때 사용합니다
    유니티 에디터에 영향을 주는 작업을 진행할 경우 해당 위치에서 작업합니다
    (플러그인 코드)
    
    3. Resources
    씬에서 게임 플레이에 사용할 에셋의 인스턴스를 만들지 않고 스크립트에서
    온디멘드(On-Demand) 방식으로 에셋을 로드해서 사용하기 위한 폴더
    
    온디멘드 : 이용자의 요구에 따라 상품이나 서비스가 바로 제공되는 것,
    이용자의 위치로 상품과 서비스를 전달하는 작업
    
    에셋 로드를 런타임에서 진행함으로써 프로젝트에 배치하지 않아 프로젝트 자체의 용량은 줄이고 내부에서 다운로드를 진행하게 하는 용도로도 사용됩니다.(에셋 번들)
    
    리소스 폴더를 활용하면 다운로드 / 버전 패치
    
    4. StreamingAssets(안드로이드에서는 바로 읽을 수 없어서 설정이 필요함)
    어플리케이션의 데이터 경로 폴더
    
    -------
    json 파일
    (java script Object Notation)
    데이터 포맷의 한 유형(데이터를 표시하는 방법)
    
    사용 이유
    json 파일이 가지고 있는 데이터를 받아서
    객체나 변수에 할당해서 사용하는 용도
    
    json 파일에 따로 저장해두기 때문에 종료해도 데이터가 남는다라는 특징이 있고
    PlayerPrefs와 달리 레지스트리가 아닌 그냥 파일 자체에 보관하고 있습니다.
    
    json 파일은 오브젝트와 배열로 구성되어있으며
    오브젝트는 name / value의 순서쌍으로 관리됩니다
    { } 로 정의합니다.
    
    배열의 경우는 [ ] 로 사용합니다

--- UIExample/Assets/InfoManager.cs
+++ UIExample/Assets/InfoManager.cs
@@ -1,18 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO; // File, Directory 사용을 위한 using
 
 public class InfoManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    Info playerInfo;
+
+    public Text id;
+    public Text point;
+    public Text gold;
+
+
+    private void Awake()
     {
-        
+        var loadedJson = Resources.Load<TextAsset>("info");
+        //리소스 폴더에 있는 Info(TextAsset) 로드 하겠습니다.
+        //Text Asset은 텍스트 형태의 에셋을 의미합니다.
+        Debug.Log(loadedJson.ToString());
+
+        playerInfo = JsonUtility.FromJson<Info>(loadedJson.text);
+        //JsonUtility.FromJson<T>(string json);
+        //json 파일로부터 읽어온 파일을 기준으로 데이터를 적용하는 코드
+        id.text = playerInfo.name.ToString();
+        point.text = playerInfo.point.ToString();
+        gold.text = playerInfo.gold.ToString();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 포인트를 사용해서 골드로 변경하는 코드 ( 100 포인트 > 10,000 골드 )
+    /// </summary>
+    public void GoldPlus()
     {
-        
+        if (playerInfo.point >= 100)
+        {
+            playerInfo.point -= 100;
+            playerInfo.gold += 10000;
+            SaveData(playerInfo);
+
+
+            //var classToJson = JsonUtility.ToJson(playerInfo);
+            point.text = playerInfo.point.ToString();
+            gold.text = playerInfo.gold.ToString();
+            //JsonUtility.ToJson(object obj);
+            //객체의 정보를 json 파일로 보내는 기능
+            //플레이어 정보를 json 파일에 전달
+
+        }
+        else
+        {
+            Debug.Log("교환할 포인트가 부족합니다");
+        }
+    }
+
+    private string ResourcePath = Application.dataPath + "/Resources/";
+
+    //[ 유니티 데이터 경로]
+    private string SavePath => Application.persistentDataPath;
+    //쓰기 가능한 폴더의 위치, 특정 운영체제에서 앱이 사용할 수 있도록 허용하는 경로
+    //C:\Users\[user name]\AppData\LocalLow\[company name]\[product name]
+
+    private string DataPath = Application.dataPath;
+    //데이터의 저장 경로(읽기 전용)으로 프로젝트 폴더 내부(Asset)을 의미합니다
+
+    private string StreamingPath = Application.streamingAssetsPath;
+    //Application.dataPath + StreamingAssets = Application.treamingAssetsPath
+
+    public void SaveData(Info info)
+    {
+        //폴더가 없을 경우에는 폴더를 생성합니다.
+        if (!Directory.Exists(ResourcePath)) Directory.CreateDirectory(ResourcePath);
+
+        var sJson = JsonUtility.ToJson(info);
+
+        var FilePath = ResourcePath + "Info.json";
+
+        File.WriteAllText(FilePath, sJson);
     }
 }
